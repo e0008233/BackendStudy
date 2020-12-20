54814c88378ae48bdeef10a3ea9236446a61c96b
e0008233 <howard.zhang.1995@gmail.com>
2020-12-20 23:02:20 +0800

    update dp

--- src/main/java/questions/algorithms/number5/Solution2.java
+++ src/main/java/questions/algorithms/number5/Solution2.java
@@ -1,5 +1,9 @@
 package questions.algorithms.number5;
 
+
+// Time complexity: O(n^2)
+// Iterating through the String, taking each character with/without the following one as the center and expanding in 2 directions
+// tracking the longest one
 public class Solution2 {
     public static String longestPalindrome(String s) {
         if (s.length()==1){
@@ -8,18 +12,18 @@ public class Solution2 {
         String result="";
 
         for (int i=0; i<s.length();i++){
-            String temp = findABA(s,i,i);
+            String temp = expand(s,i,i);
             if (temp.length()>result.length()) result= temp;
             if (i<=s.length()-2 && s.charAt(i)==s.charAt(i+1)){
 
-                String temp2 = findABA(s,i,i+1);
+                String temp2 = expand(s,i,i+1);
                 if (temp2.length()>result.length()) result= temp2;
             }
         }
         return result;
     }
 
-    private static String findABA(String s, int start, int end) {
+    private static String expand(String s, int start, int end) {
 
         String result = s.substring(start,end+1);
         while (start>=0 && end<=s.length()-1){
