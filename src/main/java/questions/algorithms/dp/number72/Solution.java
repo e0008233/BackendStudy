c94613f8b5ee933fff5c6b14867c03767a273579
e0008233 <howard.zhang.1995@gmail.com>
2020-12-29 23:33:23 +0800

    update 72

--- src/main/java/questions/algorithms/dp/number72/Solution.java
+++ src/main/java/questions/algorithms/dp/number72/Solution.java
@@ -1,7 +1,10 @@
 package questions.algorithms.dp.number72;
+//1. If last characters of two strings are same, nothing much to do. Ignore last characters and get count for remaining strings. So we recur for lengths m-1 and n-1.
+//2. Else (If last characters are not same), we consider all operations on ‘str1’, consider all three operations on last character of first string, recursively compute minimum cost for all three operations and take minimum of three values.
+//        Insert: Recur for m and n-1
+//        Remove: Recur for m-1 and n
+//        Replace: Recur for m-1 and n-1
 
-// find the max ending at each array position
-// time complexity O(n)
 public class Solution {
     public int minDistance(String word1, String word2) {
         return 0;
