ebcea6c0aa96b4e84d0fbcb603a6eead2510d8a8
e0008233 <howard.zhang.1995@gmail.com>
2020-12-30 22:47:56 +0800

    update dac

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,9 +1,10 @@
 import questions.algorithms.dac.number84.*;
 
+// Interesting question: number32, number 84
 public class main {
     public static void main(String[] args){
-        Solution solution = new Solution();
+        Solution1 solution = new Solution1();
 //        int[] input = new int[]{2,1,5,6,2,3};
-        System.out.println(solution.largestRectangleArea(new int[]{4,2}));
+        System.out.println(solution.largestRectangleArea(new int[]{3,6,5,7,4,8,1,0}));
     }
 }
