220410c448a9d9495f3caa113e0e3acbadaa430d
e0008233 <howard.zhang.1995@gmail.com>
2021-01-02 22:49:53 +0800

    update 152

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,10 +1,10 @@
-import questions.algorithms.dac.number84.*;
+import questions.algorithms.dp.number152.*;
 
 // Interesting question: number32, number 84
 public class main {
     public static void main(String[] args){
-        Solution1 solution = new Solution1();
+        Solution solution = new Solution();
 //        int[] input = new int[]{2,1,5,6,2,3};
-        System.out.println(solution.largestRectangleArea(new int[]{3,6,5,7,4,8,1,0}));
+        System.out.println(solution.maxProduct(new int[]{0,2}));
     }
 }
