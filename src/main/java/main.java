9bc5a87fa07d14c2ec8f153c7564fb03f79cf28c
e0008233 <howard.zhang.1995@gmail.com>
2021-01-03 21:53:49 +0800

    house rob problem

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,4 +1,4 @@
-import questions.algorithms.dp.number120.*;
+import questions.algorithms.dp.number198.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -7,31 +7,7 @@ import java.util.List;
 public class main {
     public static void main(String[] args){
         Solution solution = new Solution();
-//        int[] input = new int[]{2,1,5,6,2,3};
-        List<List<Integer>> triangle = new ArrayList<>();
-        List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2= new ArrayList<>();
-        List<Integer> list3 = new ArrayList<>();
-        List<Integer> list4 = new ArrayList<>();
 
-        list1.add(-1);
-        list2.add(2);
-        list2.add(3);
-        list3.add(1);
-        list3.add(-1);
-        list3.add(-3);
-//        list4.add(4);
-//        list4.add(1);
-//        list4.add(8);
-//        list4.add(3);
-
-
-        triangle.add(list1);
-        triangle.add(list2);
-        triangle.add(list3);
-//        triangle.add(list4);
-
-
-        System.out.println(solution.minimumTotal(triangle));
+        System.out.println(solution.rob(new int[]{1,2,3,1}));
     }
 }
---
+++ src/main/java/questions/algorithms/dp/number198/Solution.java
@@ -0,0 +1,22 @@
+package questions.algorithms.dp.number198;
+
+// Create an extra space dp, DP array to store the sub-problems.
+// Tackle some basic cases, if the length of the array is 0, print 0, if the length of the array is 1, print the first element, if the length of the array is 2, print maximum of two elements.
+// Update dp[0] as array[0] and dp[1] as maximum of array[0] and array[1]
+// Traverse the array from the second element to the end of array.
+// For every index, update dp[i] as maximum of dp[i-2] + array[i] and dp[i-1], this step defines the two cases, if an element is selected then the previous element cannot be selected and if an element is not selected then the previous element can be selected.
+// Print the value dp[n-1]
+public class Solution {
+    public int rob(int[] nums) {
+        if (nums.length==0) return 0;
+        if (nums.length==1) return nums[0];
+        if (nums.length==2) return Math.max(nums[0],nums[1]);
+        int[] result = new int[nums.length];
+        result[0] = nums[0];
+        result[1] = Math.max(nums[0],nums[1]);
+        for (int i=2;i<nums.length;i++){
+            result[i] = Math.max(result[i-2]+nums[i],result[i-1]);
+        }
+        return result[nums.length-1];
+    }
+}
