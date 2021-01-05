93ed1a8329bbabb1f7508781450925f299d65786
e0008233 <howard.zhang.1995@gmail.com>
2021-01-05 23:40:05 +0800

    update

---
+++ src/main/java/questions/algorithms/dp/number300/Solution.java
@@ -0,0 +1,14 @@
+package questions.algorithms.dp.number300;
+
+
+public class Solution {
+    int[] resultEndingHere;
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length==0) return 0;
+        if (nums.length==1) return 1;
+
+        int result = 0;
+
+        return result;
+    }
+}
---
+++ src/main/java/questions/algorithms/number295/Solution.java
@@ -0,0 +1,25 @@
+package questions.algorithms.number295;
+
+
+class MedianFinder {
+
+    /** initialize your data structure here. */
+    public MedianFinder() {
+
+    }
+
+    public void addNum(int num) {
+
+    }
+
+    public double findMedian() {
+      return 0.0;
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
