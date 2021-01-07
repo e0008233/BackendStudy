6edaf7dc45e66b04466ef00c53940ffb1cdd545e
e0008233 <howard.zhang.1995@gmail.com>
2021-01-08 00:22:52 +0800

    update bfs

---
+++ src/main/java/questions/algorithms/dfs/number17/Solution1.java
@@ -0,0 +1,8 @@
+package questions.algorithms.dfs.number17;
+
+
+// https://www.geeksforgeeks.org/iterative-letter-combinations-of-a-phone-number/
+// Using queue for bfs
+// using stack for dfs
+public class Solution1 {
+}
---
+++ src/main/resources/BackendStudyNotes/algorithm/Search.md
@@ -0,0 +1,8 @@
+Depth First Search (DFS):  
+- Using stack  
+- https://www.youtube.com/watch?v=iaBEKo5sM7w
+
+
+Breadth First Search (BFS):  
+- Using Queue    
+- https://www.youtube.com/watch?v=QRq6p9s8NVg  
