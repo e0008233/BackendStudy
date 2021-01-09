076d2299464dc0cb782f3b0d1cd5c19d3b3da42a
e0008233 <howard.zhang.1995@gmail.com>
2021-01-09 23:27:37 +0800

    update dfs

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,4 +1,4 @@
-import questions.algorithms.number78.*;
+import questions.algorithms.dfs.number54.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -6,8 +6,8 @@ import java.util.List;
 // Interesting question: number32(parentheses), number 84
 public class main {
     public static void main(String[] args){
-        Solution2 solution = new Solution2();
+        Solution solution = new Solution();
 
-        System.out.println(solution.subsets(new int[]{2,4,1}));
+        System.out.println(solution.spiralOrder(new int[][]{{1,2,3,4},{5,6,7,8},{9,10,11,12}}));
     }
 }
