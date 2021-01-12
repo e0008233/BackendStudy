d5cbc2b495af218ca1221558dbef3a41dbf49b63
e0008233 <howard.zhang.1995@gmail.com>
2021-01-12 23:03:37 +0800

    one day off

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 // Interesting question: number32(parentheses), number 84
+
 public class main {
     public static void main(String[] args){
         Solution solution = new Solution();
