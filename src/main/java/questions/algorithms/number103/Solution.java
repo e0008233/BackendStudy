c971fe29946fe62f43d77c6e72b4da024f36aa0b
e0008233 <howard.zhang.1995@gmail.com>
2021-01-15 23:57:41 +0800

    update

--- src/main/java/questions/algorithms/number103/Solution.java
+++ src/main/java/questions/algorithms/number103/Solution.java
@@ -11,6 +11,22 @@ import java.util.Queue;
 
 public class Solution {
     public void flatten(TreeNode root) {
-        
+        if (root==null) return;
+        if(root.left==null && root.right==null) return;
+
+        if (root.left!=null){
+            TreeNode temp = root.right;
+
+            root.right=root.left;
+            root.left=null;
+            TreeNode toAdd = root.right;
+
+            while (toAdd.right!=null){
+                toAdd=toAdd.right;
+            }
+            toAdd.right=temp;
+        }
+
+        flatten(root.right);
     }
 }
