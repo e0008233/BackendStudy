146a237dd26121bc6b8177d7eef7d86367dc6343
e0008233 <howard.zhang.1995@gmail.com>
2021-01-18 23:32:22 +0800

    update redis

---
+++ src/main/java/questions/algorithms/dac/number241/Solution.java
@@ -0,0 +1,13 @@
+package questions.algorithms.dac.number241;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//
+public class Solution {
+    public List<Integer> diffWaysToCompute(String input) {
+        List<Integer> ans= new ArrayList<>();
+
+        return ans;
+    }
+}
