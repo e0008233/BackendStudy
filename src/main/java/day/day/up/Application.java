c545881f216c0973be431315dabd6b96dd9a529b
zhang hao <zhanghao@mozat.com>
2021-02-11 10:23:34 +0800

    update

--- src/main/java/day/day/up/Application.java
+++ src/main/java/day/day/up/Application.java
@@ -1,5 +1,6 @@
 package day.day.up;
 
+import day.day.up.programming.jdbc.Database;
 import day.day.up.programming.upload.ComplexCsv2DbInserter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,23 +13,25 @@ public class Application {
 //        Sort solution = new Sort();
 //        solution.sort();
 //        System.out.println();
-        String fileName = "";
-        int start = 0;
-        int end = 0;
-        int choice = 0;
-        int size = 0;
-        String path = "";
-        if (args.length > 0) {
-            path=args[0];
-            fileName=args[1];
-            start=Integer.parseInt(args[2]);
-            end=Integer.parseInt(args[3]);
-            choice = Integer.parseInt(args[4]);
-            size = Integer.parseInt(args[5]);
+//        String fileName = "";
+//        int start = 0;
+//        int end = 0;
+//        int choice = 0;
+//        int size = 0;
+//        String path = "";
+//        if (args.length > 0) {
+//            path=args[0];
+//            fileName=args[1];
+//            start=Integer.parseInt(args[2]);
+//            end=Integer.parseInt(args[3]);
+//            choice = Integer.parseInt(args[4]);
+//            size = Integer.parseInt(args[5]);
+//
+//        }
+//        System.out.println(fileName+" " + start+" "+ end);
+//        ComplexCsv2DbInserter.upload(path,fileName,start,end,choice,size);
+        Database.updateDatabase();
 
-        }
-        System.out.println(fileName+" " + start+" "+ end);
-        ComplexCsv2DbInserter.upload(path,fileName,start,end,choice,size);
         // backend
 //        SpringApplication.run(Application.class, args);
     }
---
+++ src/main/java/day/day/up/programming/jdbc/Database.java
@@ -0,0 +1,61 @@
+package day.day.up.programming.jdbc;
+
+import java.sql.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Database {
+    public static void updateDatabase(){
+        String jdbcURL = "";
+        String username = "";
+        String password = "";
+
+        Map<String,Integer> toAdd = new HashMap<>();
+        Connection connection = null;
+
+        String query ="SELECT pi, COUNT(*) FROM rings_account.account  where register_time >= '2021-02-04 10:00:00' GROUP BY pi;";
+        try {
+            connection = DriverManager.getConnection(jdbcURL, username, password);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        try (Statement stmt = connection.createStatement()) {
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                String pi = rs.getString("pi");
+                int count = rs.getInt("COUNT(*)");
+                toAdd.put(pi,count);
+            }
+        } catch (SQLException e) {
+           System.out.println(e);
+        }
+        System.out.println("Read all data from account");
+
+
+        try {
+            String sql = "INSERT INTO account_device (pi, num) VALUES (?, ?) ON DUPLICATE KEY UPDATE num=num+VALUES(num);";
+
+            PreparedStatement statement = connection.prepareStatement(sql);
+            int mapCount = 0;
+            int batchSize = 100;
+            for (String key : toAdd.keySet()) {
+                mapCount++;
+                statement.setString(1, key);
+                statement.setInt(2, toAdd.get(key));
+                statement.addBatch();
+
+                if (mapCount % batchSize == 0) {
+                    System.out.println("committing the batch of "+batchSize+": "+ mapCount);
+                    statement.executeBatch();
+                    statement.clearBatch();
+                }
+            }
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        System.out.println("Success");
+
+    }
+}
