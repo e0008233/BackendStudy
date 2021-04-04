4020220884d29f8d8dce3e877aa915bbfb1b4f15
e0008233 <howard.zhang.1995@gmail.com>
2021-04-04 22:27:49 +0800

    leetcode 200

---
+++ src/main/java/day/day/up/questions/algorithms/search_bfs_dfs_backtrack/number934/Solution.java
@@ -0,0 +1,11 @@
+package day.day.up.questions.algorithms.search_bfs_dfs_backtrack.number934;
+
+// multi-source, multi-destination shortest path
+// use dfs to find one island (connected component)
+// mark all nodes of that island to 2
+// use bfs to expand one island (marking as 2 ), push all to queue until another one is reached
+class Solution {
+    public int shortestBridge(int[][] A) {
+        return 0;
+    }
+}
