9317dafde2577c637a391c3ded33ec044e0a48c9
zhang hao <zhanghao@mozat.com>
2021-04-05 16:51:31 +0800

    leetcode 15

--- src/main/java/day/day/up/Application.java
+++ src/main/java/day/day/up/Application.java
@@ -4,7 +4,7 @@ import day.day.up.programming.jdbc.Database;
 import day.day.up.programming.jdbc.Database3;
 import day.day.up.programming.upload.ComplexCsv2DbInserter;
 import day.day.up.programming.upload.SimpleUpload;
-import day.day.up.questions.algorithms.search_bfs_dfs_backtrack.number934.Solution;
+import day.day.up.questions.algorithms.two_pointer.number15.Solution;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -61,7 +61,7 @@ public class Application {
 //        int currentDay = currentDate.getDayOfMonth();
 //        int endTime = (int)currentDate.atStartOfDay(ZoneId.of(zone)).toEpochSecond();
 
-        System.out.println(solution.shortestBridge(new int[][]{{1,1,1,1,1},{1,0,0,0,1},{1,0,1,0,1},{1,0,0,0,1},{1,1,1,1,1}}));
+        System.out.println(solution.threeSum(new int[]{-1,0,1,2,-1,-4}));
 //        Database3.updateDatabase(path,choice);
 //        SpringApplication.run(Application.class, args);
 
---
+++ src/main/java/day/day/up/questions/algorithms/two_pointer/number15/Solution.java
@@ -0,0 +1,45 @@
+package day.day.up.questions.algorithms.two_pointer.number15;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums.length==0) return ans;
+        Arrays.sort(nums);
+        for (int i = 0; i<nums.length-2;i++){
+            int twoSum = 0 - nums[i];
+
+            // ignore duplicates
+            if (i>0 && nums[i] == nums[i-1]) continue;
+
+            // two pointer to find the pair
+            int leftPointer = i+1;
+            int rightPointer = nums.length-1;
+            while (leftPointer<rightPointer){
+                int sum = nums[leftPointer] + nums[rightPointer];
+                if (sum==twoSum){
+                    List<Integer> toAdd = new ArrayList<>();
+                    toAdd.add(nums[i]);
+                    toAdd.add(nums[leftPointer]);
+                    toAdd.add(nums[rightPointer]);
+                    ans.add(toAdd);
+                    int tempLeft = leftPointer;
+                    leftPointer++;
+                    while(leftPointer<=rightPointer && nums[tempLeft] == nums [leftPointer]){
+                        leftPointer++;
+                    }
+                } else if (sum<twoSum) {
+                    leftPointer++;
+                }
+                else{
+                    rightPointer--;
+                }
+            }
+        }
+        return ans;
+    }
+}
