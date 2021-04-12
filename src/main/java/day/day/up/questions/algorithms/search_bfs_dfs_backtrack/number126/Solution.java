1a88777dfc2a0db1e123b0691aae6e7aad29aee2
e0008233 <howard.zhang.1995@gmail.com>
2021-04-12 23:16:36 +0800

    leetcode 934

---
+++ src/main/java/day/day/up/questions/algorithms/search_bfs_dfs_backtrack/number126/Solution.java
@@ -0,0 +1,14 @@
+package day.day.up.questions.algorithms.search_bfs_dfs_backtrack.number126;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Solution {
+    public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        List<List<String>> ans = new ArrayList<>();
+
+        return ans;
+    }
+}
