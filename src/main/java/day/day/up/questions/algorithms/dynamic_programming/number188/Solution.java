4c22c741d28d1b889169b1c4d419312cf906357b
e0008233 <howard.zhang.1995@gmail.com>
2021-05-06 23:39:40 +0800

    leetcode 416

---
+++ src/main/java/day/day/up/questions/algorithms/dynamic_programming/number188/Solution.java
@@ -0,0 +1,12 @@
+package day.day.up.questions.algorithms.dynamic_programming.number188;
+
+
+// https://hashnode.com/post/best-time-to-buy-and-sell-stocks-a-comprehensive-guide-cko8xg43m0oz194s114kz71pn
+public class Solution {
+    public int maxProfit(int k, int[] prices) {
+        if (k == 0 || prices.length < 2) return 0;
+
+        return 0;
+
+    }
+}
