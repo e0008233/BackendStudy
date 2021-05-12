1b8bcb10ec3648289e7b6c92e12fddb547011b35
zhang hao <zhanghao@mozat.com>
2021-05-13 00:08:59 +0800

    leetcode

--- src/main/java/day/day/up/Application.java
+++ src/main/java/day/day/up/Application.java
@@ -8,7 +8,7 @@ import day.day.up.programming.jdbc.Database3;
 import day.day.up.programming.lock.Test;
 import day.day.up.programming.upload.ComplexCsv2DbInserter;
 import day.day.up.programming.upload.SimpleUpload;
-import day.day.up.questions.algorithms.dynamic_programming.number650.Solution;
+import day.day.up.questions.algorithms.divide_and_conquer.number312.Solution;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -67,7 +67,7 @@ public class Application {
 //        int endTime = (int)currentDate.atStartOfDay(ZoneId.of(zone)).toEpochSecond();
 //        SimpleUpload.upload();
             Solution solution = new Solution();
-        System.out.println(solution.minSteps(24));
+        System.out.println(solution.maxCoins(new int[]{3,1,5,8}));
 //        QrCodeService qrCodeService = new QrCodeServiceImpl();
 //        try {
 //            qrCodeService.generateQrCode();
