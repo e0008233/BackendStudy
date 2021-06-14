28abb9996c9f884718ac2912fa5e4e1bd67d7917
zhang hao <zhanghao@mozat.com>
2021-06-14 18:01:02 +0800

    leetcode

---
+++ src/main/java/day/day/up/questions/algorithms/data_structure/tree/number101/Solution.java
@@ -0,0 +1,10 @@
+package day.day.up.questions.algorithms.data_structure.tree.number101;
+
+import day.day.up.questions.algorithms.number102.TreeNode;
+
+
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return true;
+    }
+}
