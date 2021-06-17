2436143c5edcdf46bcf5ba0d0cbc5471e199137a
zhang hao <zhanghao@mozat.com>
2021-06-17 21:48:41 +0800

    leetcode

---
+++ src/main/java/day/day/up/questions/algorithms/data_structure/tree/number637/Solution.java
@@ -0,0 +1,31 @@
+package day.day.up.questions.algorithms.data_structure.tree.number637;
+
+import day.day.up.questions.algorithms.number102.TreeNode;
+
+import java.util.*;
+
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> ans = new ArrayList<>();
+        Queue<TreeNode> level  = new LinkedList<>();
+
+        if (root==null) return ans;
+        level.add(root);
+
+        while (!level.isEmpty()){
+            int size = level.size();
+            long sum = 0;
+            long count = 0;
+            for (int i=0;i<size;i++){
+                TreeNode node = level.remove();
+                sum = sum+ node.val;
+                count=count+1;
+                if (node.left!=null) level.add(node.left);
+                if (node.right!=null) level.add(node.right);
+            }
+            if (count>0l) ans.add((double)(sum/count));
+        }
+        return ans;
+
+    }
+}
