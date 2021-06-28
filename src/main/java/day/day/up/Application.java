2a5210b42e9ced36209ee1bea634e6f5abaa65c5
zhang hao <zhanghao@mozat.com>
2021-06-28 15:29:30 +0800

    leetcode

--- src/main/java/day/day/up/Application.java
+++ src/main/java/day/day/up/Application.java
@@ -13,6 +13,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import day.day.up.programming.comparator.Sort;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 import java.io.IOException;
 import java.time.Instant;
